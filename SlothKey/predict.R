3958c3661577f125241a6a57af1dec57290f9fda
Michael Berger <vo1kod4v@gmail.com>
2019-01-31 14:51:09 +0200

    move predict.r file to SlothKey directory

--- SlothKey/predict.R
+++ SlothKey/predict.R
@@ -2,7 +2,7 @@
 
 library(stringi)
 
-constructQuery <- function( search = c('said', 'first', 'quarter' ,'profit'), ngrO = 5)
+constructQuery <- function( search = c('said', 'first', 'quarter' ,'profit'), ngrO = 5, lim = 0)
 {
   
   if(length(search)+1<ngrO) {warning("Warning! length of search can't be lower than ngrO-1 \n")}
@@ -11,23 +11,26 @@ constructQuery <- function( search = c('said', 'first', 'quarter' ,'profit'), ng
   #select
   #pselect <-paste(paste("w", ngrs,".word as wd", ngrs, sep = "", collapse = ", "), ", gram", ngrO, ".freq", sep = "")
   pselect <-paste(paste("w", ngrO,".word as wd", ngrO, sep = "", collapse = ", "), ", gram", ngrO, ".freq", sep = "")
-  pfrom   <-paste("from gram", ngrO, sep = "")
+  #from
+  pfrom   <-paste("gram", ngrO, sep = "")
   pjoin   <-paste("join words w", ngrs, " on (w", ngrs, ".id == gram",ngrO,".word", ngrs,")",sep = "",collapse = "\n" )
   #"where"
-  psearch = paste("'", search, "'", sep = "")
+  psearch <- paste("\"", search, "\"", sep = "")
   pwhere  <-paste("w",seq_along(psearch), ".word = ", psearch, sep = "", collapse = " and ")
+  plim    <- if(lim){paste("limit", lim)}else{""} 
   
-  q <- paste("select", pselect, pfrom, pjoin, "where", pwhere, sep = "\n")
-  
+  q <- paste("select", pselect,"from", pfrom, pjoin, "where", pwhere,"order by freq desc", plim , sep = "\n")
+ # TODO: limit n with max freq
   return(q)
 }
 
+#writeLines( constructQuery())
 
 vocabCheck <- function(y)
 {
   #Check input against vocabulary and drop all the words that are not in it.
   #Also, take only 4 last words, since the model is limited to 5-grams.
-  wat <- paste( "'", y, "'", sep="", collapse =",")
+  wat <- paste( "\"", y, "\"", sep="", collapse =",")
   ans <- sqldf(paste("select word from words where word in (",wat,")"),dbname = dbname )
   #tryCatch(, error = browser())
   ans <- ans$word
@@ -53,36 +56,44 @@ predict.word <-function(x, n = 3)
     # return n most common words
     return(mostFreqWords(n))
     }
-  x<-tokenize(x)
-  x <- gsub('\'', '\'\'', x[[1]], perl=T) 
+  x<-tokenize(x)[[1]]
+  # x <- gsub('\'', '\'\'', x[[1]], perl=T) 
   searchTerm <-vocabCheck(x)
   if (identical(searchTerm,character(0))){
     return(mostFreqWords(n))}
   
   ngrO <- length(searchTerm)+1
   alp <-.4
-  S <- data.table(pred = character(),score = integer(), ngram = numeric() ,stringsAsFactors = F)
+  SS <- data.table(pred = character(),score = integer(), ngram = numeric())
   for(i in ngrO:2)
   {
-    wi <- sqldf(constructQuery(search = searchTerm, i),dbname =dbname)
+    wi <- sqldf(constructQuery(search = searchTerm, i,n),dbname =dbname)
     if(nrow(wi)<1){
       #just move on
     }else{
-      wi_1 <- sqldf(constructQuery(search = searchTerm, i-1),dbname =dbname)
+      wi_1 <- sqldf(constructQuery(search = searchTerm, i-1,n),dbname =dbname)
       num<- paste("wd",i,sep="")
-      s<-data.table(pred = wi[[num]],score =(wi$freq/wi_1$freq*alp^(ngrO-i)), ngram = rep(i,nrow(wi)),stringsAsFactors = F)
-      S<-rbind(S,s)
+      S<-data.table(pred = wi[[num]],score =(wi$freq/wi_1$freq)*alp^(ngrO-i), ngram = rep(i,nrow(wi)))
+      SS<-rbind(SS,S)
+      if(nrow(SS)>n)
+      {
+        break
+      }
     }
     searchTerm <-searchTerm[-1] #tail(searchTerm,i-1)
   }
   #The same predictions from different ngrams may end up in S. So largest score between them is selected. 
   #Also, ordered by descending score.
   #SSS <<- sqldf("select pred, max(score)as score, max(ngram) as ngram  from S group by pred order by score desc")
-  SSS <<- S[order(-score),.(score = max(score)),by=.(pred)]
+
+  if(nrow(SS)>0){SS <- SS[order(-score),.(score = max(score)), by=.(pred)]}
   #SSS <<- S[.(scr),.(scr = max(score)),by=.(pred)]
 
+  sscore <- c(SS$score[-1],0)
+  # if(any(!(SS$score >= sscore))) {
+  #   stop("Error! SS is not sorted! ")}
 
-  ret <- SSS[1:n,pred]
+  ret <- SS[1:n,pred]
     if(any(is.na(ret)))
   {
       replc<-mostFreqWords(n)
@@ -93,8 +104,10 @@ predict.word <-function(x, n = 3)
   
 }
 
+# x <- "I like how the same people are in almost all of Adam Sandler's" 
+# system.time({ a <- predict.word(x,10)})
+# print(a)
 
-# x <- "said first quarter profit"
 # x <- "said first"
 #profvis({predict.word(x)})
  
